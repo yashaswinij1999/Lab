177e93cb8b8f8af6db5bb40a0c247989c03e214c
Yashaswini J <yashaswinij1999>
2023-04-30 20:05:14 +0530

    Q9.java

---
+++ Collection/Lab4/Q9.java
@@ -0,0 +1,34 @@
+public class Q9 {
+
+    static int[] findMinMax(int[] arr, int n) {
+
+        // Without using Library methods
+
+        int mini = arr[0];
+        int maxi = arr[0];
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < mini) {
+                mini = arr[i];
+            } else if (arr[i] > maxi) {
+                maxi = arr[i];
+            }
+        }
+        int[] ans = new int[2];
+        ans[0] = mini;
+        ans[1] = maxi;
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 6, 3, 7, 14 };
+        int N = arr.length;
+
+        // Function call
+        int[] ans = findMinMax(arr, N);
+        System.out.print("Maximum is: " + ans[1]);
+        System.out.print("\n"
+                + "Minimum is: " + ans[0]);
+
+    }
+}
