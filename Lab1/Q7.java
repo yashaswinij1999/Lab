455e02c1fd1e52cbad4463627556bce5acb3bc31
Yashaswini J <yashaswinij1999>
2023-05-07 13:46:54 +0530

    added

---
+++ Lab1/Q7.java
@@ -0,0 +1,14 @@
+public class Q7 {
+
+    public static void main(String[] args) {
+        
+        for(int i = 1 ; i <= 100; i++){
+
+            if(i % 3 != 0){
+                System.out.println(" non multiples of 3 = " + i);
+            }
+        }
+
+    }
+    
+}
