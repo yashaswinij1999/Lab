06598b5b817faf301aabde4192ae054e1ac37a86
Yashaswini J <yashaswinij1999>
2023-05-07 12:37:26 +0530

    added

---
+++ Lab1/A.java
@@ -0,0 +1,18 @@
+public class A {
+    
+    public static void main(String[] args) {
+        System.out.println("A.java");
+    }
+}
+ class B {
+
+    public static void main(String[] args) {
+        System.out.println("B.java");
+    }
+    
+}
+ class C {
+    public static void main(String[] args) {
+        System.out.println("C.java");
+    }
+}
