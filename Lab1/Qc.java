ddb63deaf3d09ce110e818f52e5bd37b40fdc364
Yashaswini J <yashaswinij1999>
2023-05-07 12:41:12 +0530

    added

---
+++ Lab1/Qc.java
@@ -0,0 +1,11 @@
+public class Qc {
+
+    protected static void main(int [] args) {
+        
+    }
+    
+}
+
+/* it fails because we are unable to find the main method as below
+-  please define the main method as:
+ - public static void main(String[] args) */
