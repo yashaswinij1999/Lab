8bec82eb84bddbafd3dab98de306d36f514c8d1f
Yashaswini J <yashaswinij1999>
2023-05-07 17:09:04 +0530

    added

---
+++ Lab2/Q4.java
@@ -0,0 +1,12 @@
+public class Q4 {
+
+    public static void main(String[] args) {
+
+        byte b = 100;
+        short c = 100;
+        short result = (short) (b + c);
+        System.out.println("result = " + result);
+
+    }
+
+}
