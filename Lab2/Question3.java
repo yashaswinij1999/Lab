b0cdeeb238206cb76fe088fbd3fdb1f8bce8d8aa
Yashaswini J <yashaswinij1999>
2023-05-07 17:45:33 +0530

    added

---
+++ Lab2/Question3.java
@@ -0,0 +1,18 @@
+public class Question3 {
+
+    public static void main(String[] args) {
+
+        printTables(9);
+    }
+
+    public static void printTables(int a) {
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println(a + " * " + i + " = " + a * i);
+
+        }
+
+    }
+
+}
