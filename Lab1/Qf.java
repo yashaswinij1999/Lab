896c1623ed0c96c4939b1283f0b84a02aeac1d16
Yashaswini J <yashaswinij1999>
2023-05-07 12:54:16 +0530

    added

---
+++ Lab1/Qf.java
@@ -0,0 +1,15 @@
+public class Qf {
+
+    public static void main(String[] args) {
+        
+        byte b = 50;
+        int i = b;
+        //  it is possible because byte can be stored in int
+
+        int x = 20;
+        byte c = x;
+        // int cannot be converted to byte by complier 
+
+    }
+    
+}
