4a698e7f5157ce8e67dcc4723a9466ef8862a7ed
Yashaswini J <yashaswinij1999>
2023-05-07 12:50:24 +0530

    added

---
+++ Lab1/Qe.java
@@ -0,0 +1,16 @@
+public class Qe {
+
+    public static void main(String[] args) {
+
+        byte b = 100;
+        System.out.println(" b =====> " + b);
+
+        // byte c = 128;
+        // byte can store form -128 to 127;
+
+        byte d =-128;
+        System.out.println( " d =====> " + d);
+
+    }
+
+}
