ca5698a2ec5db2176ba3031c59b4ef0d13a34c59
Yashaswini J <yashaswinij1999>
2023-05-07 13:21:31 +0530

    added

---
+++ Lab1/Qm.java
@@ -0,0 +1,11 @@
+public class Qm {
+
+    public static void main(String[] args) {
+        
+        byte b = 100;
+        byte c = 100;
+        byte d = (byte) (b + c);
+        System.out.println("d===> "+ d);
+    }
+    
+}
