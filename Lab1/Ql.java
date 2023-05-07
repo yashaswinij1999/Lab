85329ccf86db436dbde73eda3dcd443c1ad673d4
Yashaswini J <yashaswinij1999>
2023-05-07 13:18:33 +0530

    added

---
+++ Lab1/Ql.java
@@ -0,0 +1,13 @@
+public class Ql {
+
+    public static void main(String[] args) {
+        // int a = 10/0; 
+        // double d = 10.0/0.0;
+        // float f = 10.0f/0.0f;
+        double d = 10.0/0;
+        double a = 10/0.0;
+
+        System.out.println("a===>" + a);
+    }
+
+}
