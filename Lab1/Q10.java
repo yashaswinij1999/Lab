2aa512bf5ced4a5ef93ce3bad5313700061c385a
Yashaswini J <yashaswinij1999>
2023-05-07 14:08:59 +0530

    added

---
+++ Lab1/Q10.java
@@ -0,0 +1,14 @@
+public class Q10 {
+
+    public static void main(String[] args) {
+        
+        System.out.println(100 << 2);
+        System.out.println(100 << 4);
+        System.out.println(100 << 8);
+        System.out.println(100 >> 2);
+        System.out.println(100 >> 4);
+        System.out.println(100 >> 8);
+
+    }
+    
+}
