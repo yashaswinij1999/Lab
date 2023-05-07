8a54c8386c0b928e4560dae838ad2758e6356b9d
Yashaswini J <yashaswinij1999>
2023-05-07 17:06:39 +0530

    added

---
+++ Lab2/Q3.java
@@ -0,0 +1,14 @@
+public class Q3 {
+
+    public static void main(String[] args) {
+        
+
+        char c = 'a' + 100;
+        int i = c ;
+        System.out.println("c===> " + c);
+        System.out.println("i===> " + i);
+
+
+    }
+    
+}
