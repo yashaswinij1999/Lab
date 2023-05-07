38f2966e837d4be561033514e5f198947b99b15b
Yashaswini J <yashaswinij1999>
2023-05-07 13:14:03 +0530

    added

---
+++ Lab1/Qj.java
@@ -0,0 +1,15 @@
+public class Qj {
+
+    public static void main(String[] args) {
+
+        byte b = 10;
+        b = (byte) (b + 10);
+        System.out.println(b);
+
+        byte c = 10;
+        c += 10;
+        System.out.println(c);
+
+    }
+
+}
