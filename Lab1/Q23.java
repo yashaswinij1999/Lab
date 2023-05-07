62a1c2305fd864de97b819d2c9991c47fb00834f
Yashaswini J <yashaswinij1999>
2023-05-07 14:47:55 +0530

    added

---
+++ Lab1/Q23.java
@@ -0,0 +1,12 @@
+public class Q23 {
+
+    public static void main(String[] args) {
+
+        int i = 50;
+        while (i != 0) {
+            System.out.println(i >>>= 1);
+        }
+
+    }
+
+}
