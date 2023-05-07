df24936fa889fc37a384ce916df4a5cd49f24dc1
Yashaswini J <yashaswinij1999>
2023-05-07 13:03:39 +0530

    added

--- Lab1/Qh.java
+++ Lab1/Qh.java
@@ -1,3 +1,21 @@
 public class Qh {
-    
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 4;
+
+        int result1 = x + y;
+        int result2 = x * y;
+        int result3 = x / y;
+        int result4 = x % y;
+        int result5 = x - y;
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+
+    }
+
 }
