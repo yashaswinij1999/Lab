d351faa711ca38e04a325e92a3cde89476b4aced
Yashaswini J <yashaswinij1999>
2023-05-07 12:46:49 +0530

    added

---
+++ Lab1/Qd.java
@@ -0,0 +1,16 @@
+public class Qd {
+
+    public static void main(String[] args) {
+        
+ 
+     int i = 5;
+     System.out.println(" i === " + i);
+
+     int j;
+    //  System.out.println(" j======> " + j );
+
+     // local variable must be intialised;
+
+    }
+    
+}
