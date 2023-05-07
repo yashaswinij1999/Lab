38e89657f3373235720ee939652ec6be09ec6921
Yashaswini J <yashaswinij1999>
2023-05-07 14:14:08 +0530

    added

---
+++ Lab1/Q12.java
@@ -0,0 +1,12 @@
+public class Q12 {
+
+    public static void main(String[] args) {
+        
+        byte b = (byte)(127 + 10);
+        System.out.println("b===> " + b);
+
+        // data loss because int cannot be stored in byte
+
+    }
+    
+}
