26e1b64a3bf742aee5230265d5750afdc2bde717
Yashaswini J <yashaswinij1999>
2023-05-07 14:11:01 +0530

    added

---
+++ Lab1/Q11.java
@@ -0,0 +1,15 @@
+public class Q11 {
+
+    public static void main(String[] args) {
+        
+
+        int a = 100;
+        int b = 20;
+        int remainder = a % b;
+        int quoitent = a / b ;
+
+        System.out.println(" reminder = " + remainder);
+        System.out.println(" quoitent = " + quoitent);
+    }
+    
+}
