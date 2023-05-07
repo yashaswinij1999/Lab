e4bf8ef316ab81dd0ac2b1e75d2728ed3d04b17e
Yashaswini J <yashaswinij1999>
2023-05-07 14:52:28 +0530

    added

---
+++ Lab1/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        System.out.println("x ==> " + x);
+        process(x);
+
+    }
+
+    public static void process(int a) {
+
+         a = 20;
+        System.out.println("a ==> " + a);
+
+    }
+
+}
