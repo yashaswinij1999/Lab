dd698a42bb5776e67a920f09290572f4ff3ad4ba
Yashaswini J <yashaswinij1999>
2023-05-07 13:01:20 +0530

    added

---
+++ Lab1/Qg.java
@@ -0,0 +1,29 @@
+public class Qg {
+
+    public static void main(String[] args) {
+
+        int x = 20;
+        byte b = (byte) x;
+        System.out.println("b====>" + b);
+
+        short s = 10;
+        byte a = (byte)s;
+        System.out.println("a====>" + a);
+
+        char c = 47;
+        byte d = (byte)c;
+        System.out.println("d====>" + d);
+
+        long l = 14587;
+        int i = (int)l;
+        System.out.println("i===> " + i);
+
+        float f = 202514;
+        int g = (int)f;
+        System.out.println("g===>" + g);
+
+
+
+    }
+
+}
