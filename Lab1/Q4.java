0d40fdd7d3051218a5f440d9d7de57a9c0eb958c
Yashaswini J <yashaswinij1999>
2023-05-07 13:39:07 +0530

    added

---
+++ Lab1/Q4.java
@@ -0,0 +1,34 @@
+public class Q4 {
+
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 10; i++) {
+
+            // System.out.println(" using for loop " + i);
+
+        }
+
+
+
+        int i = 1;
+        while (i <= 10) {
+
+            // System.out.println("using while loop = " + i);
+            i++;
+
+        }
+
+
+
+        int j = 1;
+        do {
+            System.out.println(" using do while " + j);
+            j++;
+        }
+
+        while (j <= 10);
+
+    }
+
+}
