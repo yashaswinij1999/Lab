40aef7c30a6a282709bc0e61e98f424b4dd261ac
Yashaswini J <yashaswinij1999>
2023-05-07 17:30:18 +0530

    added

--- Lab2/Q10.java
+++ Lab2/Q10.java
@@ -7,6 +7,13 @@ public class Q10 {
         System.out.println(1 == 1 ? true : false);
         System.out.println(1 == 2 ? false : true);
 
+        System.out.println(true ? false : true);
+        System.out.println(true ? true : false);
+        char x = 'X';
+        int i = 0;
+        System.out.println(true ? x : 0);
+        System.out.println(false ? i : x);
+
     }
 
 }
