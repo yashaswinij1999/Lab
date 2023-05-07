35e3580f8cd8d1ad7e8676ae0dce0d169e53cdca
Yashaswini J <yashaswinij1999>
2023-05-07 13:48:57 +0530

    added

---
+++ Lab1/Q8.java
@@ -0,0 +1,16 @@
+public class Q8 {
+    public static void main(String[] args) {
+
+        for(int i = 1 ; i <= 100 ;i ++){
+
+            if(i % 2 == 0){
+                System.out.println("even number = " + i);
+            }
+            else{
+                System.out.println("odd number  = " + i);
+            }
+        }
+        
+    }
+    
+}
