93e247ee44a4461fa8911f843132828317538bc4
Yashaswini J <yashaswinij1999>
2023-05-07 17:06:14 +0530

    added

---
+++ Lab2/Q2.java
@@ -0,0 +1,17 @@
+public class Q2{
+
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+
+        System.out.println(" adding 2 int values = " + (c));
+
+        double d = 569.3;
+        double e = 147.58;
+        double g = d + e ;
+        System.out.println("adding 2 doubles = " + g );
+
+    }
+}
