333d4f0c18c3174fd711c45691985fef14196c0d
Yashaswini J <yashaswinij1999>
2023-05-07 17:55:22 +0530

    added

---
+++ Lab2/TestLoop.java
@@ -0,0 +1,18 @@
+public class TestLoop {
+
+    public static void main(String[] args) {
+        
+        for(int i= 1 ;i <= 100;i++){
+
+            if(i % 11 == 0){
+                System.out.println(" ");
+            }
+            else{
+                System.out.print( " " + i);
+            }
+
+        }
+
+    }
+    
+}
