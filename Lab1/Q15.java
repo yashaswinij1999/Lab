e36aec9244f251d35cab8bb4411c4a65e42a5805
Yashaswini J <yashaswinij1999>
2023-05-07 14:26:14 +0530

    added

---
+++ Lab1/Q15.java
@@ -0,0 +1,12 @@
+public class Q15 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" 2 << 1 = " + (2 << 1));
+        System.out.println(" 6 << 2 = " + (6 << 2));
+        System.out.println(" -2 << 1 = " + (-2 << 1));
+        
+
+    }
+
+}
