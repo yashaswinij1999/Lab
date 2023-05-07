e78013f5be428b35f5900bada23b7b30af8217c0
Yashaswini J <yashaswinij1999>
2023-05-07 13:44:27 +0530

    added

---
+++ Lab1/Q6.java
@@ -0,0 +1,14 @@
+public class Q6 {
+
+    public static void main(String[] args) {
+        
+        for(int i = 1 ; i <= 100; i++){
+
+            if(i % 2 == 0){
+                System.out.println("even numbers = " + i);
+            }
+        }
+
+    }
+    
+}
