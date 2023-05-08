6a60380744d17884e5314215a7f45b83c777fdfe
Yashaswini J <yashaswinij1999>
2023-05-08 20:52:12 +0530

    added

---
+++ OOPs/Practicls3.java/Average4.java
@@ -0,0 +1,17 @@
+public class Average4 {
+
+    public static void main(String[] args) {
+
+        String s = "";
+
+        for (int i = 1; i <= 10; i++) {
+
+            int a = (int) (Math.random() * 10);
+            s = s + a;
+
+        }
+        System.out.println(s);
+
+    }
+
+}
