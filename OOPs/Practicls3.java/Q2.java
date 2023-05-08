08b8b22b72504034f8d8377e6eefaff4f3627ffd
Yashaswini J <yashaswinij1999>
2023-05-08 19:19:30 +0530

    added

---
+++ OOPs/Practicls3.java/Q2.java
@@ -0,0 +1,19 @@
+public class Q2 {
+
+    boolean b;
+    int i;
+    String str;
+
+}
+
+class TestQ2 {
+
+    public static void main(String[] args) {
+
+        Q2 q = new Q2();
+        System.out.println(" default value of boolean = " + q.b);
+        System.out.println(" default value of int = " + q.i);
+        System.out.println(" default value of String = " + q.str);
+
+    }
+}
