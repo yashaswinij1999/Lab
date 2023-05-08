033000fd6275c11323c51ab0ddd392d14e5efc59
Yashaswini J <yashaswinij1999>
2023-05-08 20:58:50 +0530

    added

---
+++ OOPs/Practicls3.java/Q9.java
@@ -0,0 +1,13 @@
+public class Q9 {
+
+    public static void main(String[] args) {
+
+
+        for(int i = 1 ; i <= 10;i++){
+
+            int a = (int)(Math.random() * 100);
+            System.out.println(a);
+        }
+    }
+    
+}
