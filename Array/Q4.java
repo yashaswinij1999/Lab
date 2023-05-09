100a70909529bd939f22f2cc75ebad40e55875d5
Yashaswini J <yashaswinij1999>
2023-05-09 16:56:34 +0530

    added

---
+++ Array/Q4.java
@@ -0,0 +1,21 @@
+public class Q4 {
+    
+
+    public static void main(String[] args) {
+
+        int [] arr = {14,1,4,7,9};
+        test(arr);
+        
+    }
+
+    public static void test(int [] arr){
+
+        for(int i = 0 ; i < arr.length ; i++){
+
+            int a = arr[i];
+            System.out.println(a);
+        }
+
+
+    }
+}
