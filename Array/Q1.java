93d370299633c4593573f0c6fc26c604b89c314d
Yashaswini J <yashaswinij1999>
2023-05-09 16:38:21 +0530

    added

---
+++ Array/Q1.java
@@ -0,0 +1,33 @@
+public class Q1 {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[10];
+        arr[1] = 12;
+        arr[2] = 15;
+        arr[3] = 16;
+        arr[4] = 17;
+        arr[5] = 19;
+        arr[6] = 21;
+        arr[7] = 22;
+        arr[8] = 23;
+        arr[9] = 24;
+        
+
+        for(int i = 0; i< arr.length;i++){
+
+            int x = arr[i];
+            // System.out.println("x == " + x);
+
+        }
+
+        int [] arr2 = {1,4,7,8,9};
+
+        for(int i = 0; i < arr2.length ; i++){
+
+            int j = arr2[i];
+            System.out.println("j ==" + j);
+        }
+
+    }
+}
