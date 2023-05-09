7de522b6902ea589704f9a13261a41cfc30af183
Yashaswini J <yashaswinij1999>
2023-05-09 16:43:18 +0530

    added

---
+++ Array/Q2.java
@@ -0,0 +1,15 @@
+public class Q2 {
+
+    public static void main(String[] args) {
+        
+        int [] arr = new int[10];
+
+        for(int i = arr.length -1 ; i > 0; i--){
+
+            arr[i] = i;
+            System.out.println(arr[i]);
+        }
+
+    }
+    
+}
