2394c32541413b14fe9b279c69e0c67ddb8edd21
Yashaswini J <yashaswinij1999>
2023-05-10 16:21:37 +0530

    added

---
+++ Strings/Q16.java
@@ -0,0 +1,15 @@
+public class Q16 {
+
+    public static void main(String[] args) {
+
+        String str = "";
+
+        for (int i = 0; i < args.length; i++) {
+
+            str = str + " " + args[i];
+        }
+        System.out.println(str);
+
+    }
+
+}
