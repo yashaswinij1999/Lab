cc6587068d44f838dc73e42db75d3023d02f49b3
Yashaswini J <yashaswinij1999>
2023-05-12 19:11:08 +0530

    added

---
+++ Constructors/Car.java
@@ -0,0 +1,73 @@
+public class Car {
+
+    private int fuel;
+    private String name;
+    boolean isStarted;
+
+    public void setName(String name) {
+
+        if (name == null || name.equals(" ")) {
+            throw new IllegalArgumentException("Invalid name ");
+        }
+        this.name = name;
+
+    }
+
+    public String getName() {
+
+        return name;
+
+    }
+
+    public void setFuel(int fuel) {
+
+        if (fuel < 0) {
+
+            throw new IllegalArgumentException("Invalid fuel Qty");
+
+        }
+        this.fuel = fuel;
+    }
+
+    public int getFuel() {
+
+        return fuel;
+
+    }
+
+    public void start() {
+
+        if (fuel > 0) {
+            isStarted = true;
+            System.out.println(" the vehicle started ");
+        }
+
+    }
+
+    public void drive() {
+
+        if (isStarted == true) {
+            System.out.println(" the car is ready to drive ");
+            fuel--;
+        }
+    }
+
+    public void reverse() {
+
+        if (isStarted == true) {
+            System.out.println(" the car reversing ");
+            fuel--;
+
+        }
+    }
+
+    public void stop() {
+
+        if (fuel < 0) {
+            isStarted = false;
+            System.out.println(" the car stopped ");
+        }
+
+    }
+}
+
