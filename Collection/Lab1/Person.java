fab86b46baad18618ed225fdeb41030bf50667bb
Yashaswini J <yashaswinij1999>
2023-05-15 12:54:02 +0530

    added

---
+++ Collection/Lab1/Person.java
@@ -0,0 +1,43 @@
+public class Person {
+
+    String name;
+    int height;
+
+    public Person(String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    public boolean equals(Object e) {
+
+        if (e instanceof Person) {
+            Person p = (Person) e;
+
+            if (this.name.equals(p.name) && this.height == p.height) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String toString() {
+        return "Person:" + name + "," + height;
+    }
+
+    public int hashCode() {
+        return (name + height).hashCode();
+    }
+
+}
+
+class TestPerson {
+
+    public static void main(String[] args) {
+
+        Person p1 = new Person("ram", 5);
+        Person p2 = new Person("ram", 5);
+        System.out.println(p1 == p2);
+        System.out.println("p1.equals(p2) = " + p1.equals(p2));
+
+    }
+}
