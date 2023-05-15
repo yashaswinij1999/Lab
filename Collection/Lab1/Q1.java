3129ee25f529e9550602250828d5c7dd64e3437a
Yashaswini J <yashaswinij1999>
2023-05-15 12:41:31 +0530

    added

---
+++ Collection/Lab1/Q1.java
@@ -0,0 +1,11 @@
+public class Q1 {
+
+    public static void main(String[] args) {
+
+        String s1 = "abc";
+        String s2 = "abc";
+        System.out.println("s1 == s2 = " + (s1 == s2));
+        System.out.println("s1.equals(s2) = " +  s1.equals(s2));
+
+    }
+}
