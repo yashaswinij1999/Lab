6da3b05d1fe366f502dd24fba9a94d2806f73d71
Yashaswini J <yashaswinij1999>
2023-05-17 15:01:09 +0530

    added

--- Collection/Lab3/Prectice 2/Q3.java
+++ Collection/Lab3/Prectice 2/Q3.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
+
+
 
 public class Q3 {
 
@@ -28,12 +31,26 @@ public class Q3 {
 
         System.out.println("no of occurance of word " + word + " = " + Collections.frequency(l, word));
 
-        Collections.sort(l, slc);
+        Collections.sort(l);
         System.out.println("sorting with duplcates = " + l);
 
         Set <String> ts = new TreeSet<>();
         ts.addAll(l);
         System.out.println("sorting without duplicates = " + ts);
+
+        Set <String> ts = new TreeSet<>(slc);
+        System.out.println(" sort using string length comparison" + ts);
+
+        Iterator<String> it = l.iterator();
+        while(it.hasNext()){
+            String val = it.next();
+
+            if(val.equals(word)){
+                it.remove();
+            }
+        }
+
+        System.out.println(" remove all the occurrences of the word = " + l);
 
     }
 
