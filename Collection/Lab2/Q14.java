62ae1b9cf3f52bf7ee683dc7531f416d55c27e28
Yashaswini J <yashaswinij1999>
2023-05-17 13:36:15 +0530

    added

---
+++ Collection/Lab2/Q14.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Q14 {
+
+    public static void main(String[] args) {
+
+        String sentence = "this this is is a what how what is ";
+        String[] split = sentence.split(" ");
+
+        // int i = 0;
+        // int j = 0;
+
+        List<String> l = new ArrayList();
+
+        for (String s : split) {
+            l.add(s);
+        }
+
+        int count = 0;
+        for (int i = 0; i < l.size(); i++) {
+
+            count = 0;
+            for (int j = 0; j < l.size(); j++) {
+
+                if (l.get(i).equals(l.get(j))) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                System.out.println("non repeating words == " + l.get(i));
+            }
+        }
+    }
+
+}
