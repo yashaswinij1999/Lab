8bc87665726f71cbb5a1bef194107d31af3543c9
Yashaswini J <yashaswinij1999>
2023-05-17 20:39:44 +0530

    added

---
+++ Collection/Lab3/Prectice 2/LinkedListStack.java
@@ -0,0 +1,75 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListStack {
+
+    int pos = 0;
+    int size = 20;
+
+    List<String> l = new LinkedList<>();
+
+    public void push(String element) {
+        if (pos > size || pos < 0) {
+            return;
+        }
+        l.add(pos, element);
+        pos++;
+        System.out.println(l);
+    }
+
+    public String pop() {
+
+        if (pos < 0 || pos > size) {
+            return null;
+        }
+        pos--;
+        String val = l.get(pos);
+        return val;
+    }
+
+    public String peek(int position) {
+        if (pos < 0) {
+            return null;
+        }
+        if (position > pos || position > size || position < 0) {
+            return null;
+        }
+        String val = l.get(position - 1);
+        return val;
+
+    }
+
+}
+
+class Q11 {
+
+    public static void main(String[] args) {
+
+        LinkedListStack l1 = new LinkedListStack();
+        LinkedListStack l2 = new LinkedListStack();
+        // l1.push("rama");
+        // l1.push("ranga");
+        // System.out.println(l1.pop());
+        // System.out.println(l1.peek(1));
+
+        String sentence = "Welcome to the World of Java";
+        String[] split = sentence.split(" ");
+
+        for (String s : split) {
+            l1.push(s);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= split.length; i++) {
+            System.out.println(l1.peek(i));
+            sb.append(l1.peek(i));
+            l2.push(sb.reverse().toString());
+            sb.delete(0, sb.length());
+        }
+
+        for (int i = 1; i <= split.length; i++) {
+            System.out.println(l2.peek(i));
+        }
+    }
+}
