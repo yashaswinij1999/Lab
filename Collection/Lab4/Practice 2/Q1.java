2136cc2e0461e3a9801c937dc8c60cbe7605543e
Yashaswini J <yashaswinij1999>
2023-05-19 09:43:44 +0530

    added

--- Collection/Lab4/Practice 2/Q1.java
+++ Collection/Lab4/Practice 2/Q1.java
@@ -7,21 +7,20 @@ public class Q1 {
 
         Map<String, Integer> m1 = new HashMap();
 
-        String sentence = "Mary had a little Lamp";
+        String sentence = "Johny Johny yes pa pa eating sugar no pa pa";
         String[] split = sentence.split(" ");
+        String word = "pa";
+        int count = 0;
 
         for (String s : split) {
-            Integer occurance = m1.get(s);
 
-            if (occurance == null) {
-                m1.put(s, 1);
-            } else {
-                m1.put(s, occurance + 1);
+            if (s.equals(word)) {
+                count++;
             }
 
         }
 
-        System.out.println("m1 ===> " + m1);
+        System.out.println("a word " + word + " occurs " + count + " no of times ");
 
     }
 }
