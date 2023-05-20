7d14be5257de7371c6154274f8d811a6ec823fc4
Yashaswini J <yashaswinij1999>
2023-05-20 17:45:47 +0530

    added

---
+++ Collection/Lab4/Practice 1/Q8.java
@@ -0,0 +1,20 @@
+
+
+public class Q8 {
+
+    public static void main(String[] args) {
+
+        // b .without Using Library methods
+
+        String[] arr = { "rama", "ranga", "lakshmi", "saraswathi", "ganga", "krishna" };
+
+        for (int j = arr.length - 1; j >= 0; j--) {
+
+            String s1 = arr[j];
+            System.out.println(s1);
+
+        }
+
+    }
+
+}
