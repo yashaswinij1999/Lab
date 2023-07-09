6a80db0e8a37a1cae924debf43dd08f7783712fa
Yashaswini J <yashaswinij1999>
2023-07-09 18:55:42 +0530

    added

---
+++ JDBC/Practice 2/JDBCHelper.java
@@ -0,0 +1,82 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JDBCHelper {
+
+    public static Connection getConnection() {
+
+        Connection con = null;
+        try {
+
+            Class.forName(Constants.DRIVER);
+            con = DriverManager.getConnection(Constants.URL, Constants.UID, Constants.PASSWORD);
+            return con;
+
+        } catch (SQLException | ClassNotFoundException e) {
+
+            // TODO: handle exception
+            e.printStackTrace();
+            return null;
+
+        }
+
+    }
+
+    public static void close(Connection con) {
+
+        try {
+
+            if (con != null) {
+                con.close();
+                System.out.println("closed");
+            }
+
+        } catch (Exception e) {
+
+            // TODO: handle exception
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public static void close(ResultSet rs) {
+
+        try {
+
+            if (rs != null) {
+                rs.close();
+                System.out.println("closed");
+            }
+
+        } catch (Exception e) {
+
+            // TODO: handle exception
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public static void close(PreparedStatement ps) {
+
+        try {
+
+            if (ps != null) {
+                ps.close();
+                System.out.println("closed");
+            }
+
+        } catch (Exception e) {
+
+            // TODO: handle exception
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
