58b2ee028e8f13c0c80cb8c1740e5ba8ff168db1
Yashaswini J <yashaswinij1999>
2023-07-09 18:55:21 +0530

    added

---
+++ JDBC/Practice 2/Constants.java
@@ -0,0 +1,8 @@
+public class Constants {
+
+    public static final String UID = "SA";
+    public static final String DRIVER = "org.hsqldb.jdbc.JDBCDriver";
+    public static final String PASSWORD = "";
+    public static final String URL = "jdbc:hsqldb:hsql://localhost/";
+
+}
