76f3421c0d5493d6dc82e61e01449f61d69f7e63
Yashaswini J <yashaswinij1999>
2023-08-02 20:32:21 +0530

    added

---
+++ Lab3/Practice 2/Q2.java
@@ -0,0 +1,17 @@
+public class Q2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(isEven(5));
+    }
+
+    public static boolean isEven(int num) {
+
+        if (num % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
