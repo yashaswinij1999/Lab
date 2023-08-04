6305dd597711c53ebc3356b755170418bb88e18d
Yashaswini J <yashaswinij1999>
2023-08-04 19:30:40 +0530

    added

---
+++ OOPs/Practicls3.java/Practice2/Q2.java
@@ -0,0 +1,20 @@
+public class Q2 {
+
+    public static void main(String[] args) {
+
+        DefaultValue d = new DefaultValue();
+        System.out.println(d.str);
+        System.out.println(d.i);
+        System.out.println(d.b);
+
+    }
+
+}
+
+class DefaultValue {
+
+    String str;
+    int i;
+    boolean b;
+
+}
