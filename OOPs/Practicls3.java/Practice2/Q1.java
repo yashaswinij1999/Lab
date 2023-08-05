92369755b67cd61b54d582f21ba9d23e146cda1b
Yashaswini J <yashaswinij1999>
2023-08-05 11:03:57 +0530

    added

---
+++ OOPs/Practicls3.java/Practice2/Q1.java
@@ -0,0 +1,32 @@
+public class Q1 {
+
+    public static void main(String[] args) {
+
+        System.out.println(oddNotDivisibleBy3(7));
+
+    }
+
+    public static boolean oddNotDivisibleBy3(int num) {
+
+        if (num <= 1) {
+            return false;
+        }
+
+        if (num % 2 == 0) {
+            return false;
+        }
+
+        if (num == 5 || num == 7) {
+            return true;
+        }
+
+        if (num % 2 != 0) {
+
+            if (num % 3 != 0) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
