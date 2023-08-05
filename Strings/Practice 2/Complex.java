7061f9a00252e87c96f3666e116267169017cdf3
Yashaswini J <yashaswinij1999>
2023-08-05 20:33:00 +0530

    added

---
+++ Strings/Practice 2/Complex.java
@@ -0,0 +1,13 @@
+public class Complex {
+
+    int realNumber;
+    int imaginary;
+
+    public Complex(int realNumber, int imaginary) {
+
+        this.realNumber = realNumber;
+        this.imaginary = imaginary;
+
+    }
+
+}
