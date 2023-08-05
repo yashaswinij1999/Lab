8cb2806cd598c0ca7494c8c7f70971c9aeea5a97
Yashaswini J <yashaswinij1999>
2023-08-05 20:15:08 +0530

    added

---
+++ Strings/Practice 2/Q15.java
@@ -0,0 +1,15 @@
+public class Q15 {
+
+    public static void main(String[] args) {
+
+        double d = Math.sqrt(20);
+
+        if (d == Math.round(d)) {
+            System.out.println("It is integer");
+        } else {
+            System.out.println("its is not Integer");
+        }
+
+    }
+
+}
