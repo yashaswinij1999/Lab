b87a2d8dfb125e59d81eb553e235e45313ec1626
Yashaswini J <yashaswinij1999>
2023-08-05 17:17:44 +0530

    added

---
+++ Array/Practice 2/Q4.java
@@ -0,0 +1,23 @@
+public class Q4 {
+
+    public static void main(String[] args) {
+
+        int[] arr = { 10, 20, 30, 40 };
+        test(arr);
+
+    }
+
+    public static void test(int[] arr) {
+
+        for (int x : arr) {
+            // System.out.println(x);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.println(arr[i]);
+
+        }
+
+    }
+}
