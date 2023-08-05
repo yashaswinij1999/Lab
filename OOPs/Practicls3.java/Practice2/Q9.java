f1a0e0318ae1a19e5d0273c8ac2c5ed1fa270c61
Yashaswini J <yashaswinij1999>
2023-08-05 10:47:31 +0530

    added

---
+++ OOPs/Practicls3.java/Practice2/Q9.java
@@ -0,0 +1,25 @@
+public class Q9 {
+
+    public static void main(String[] args) {
+
+        double result = 0.0;
+        // double result = Math.sqrt(64);
+        // double result = Math.cbrt(64);
+        // double result = Math.pow(2, 3);
+        // double result = Math.floor(9.999);
+        // double result = Math.ceil(9.009);
+        // result = Math.round(9.5);
+        // result = Math.round(9.49);
+        // result = Math.abs(+9.999);
+        // result = Math.abs(-9.999);
+
+        for (int i = 1; i <= 10; i++) {
+
+            int value = (int) (Math.random() * 100);
+            System.out.println(value);
+
+        }
+
+    }
+
+}
