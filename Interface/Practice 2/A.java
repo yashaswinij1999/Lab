55ccacaaf162b45a7f1077ca06ccc85075ffc062
Yashaswini J <yashaswinij1999>
2023-08-12 12:50:05 +0530

    modified

--- Interface/Practice 2/A.java
+++ Interface/Practice 2/A.java
@@ -1,6 +1,36 @@
 public class A {
 
-    public static final int P = 20;
+    static String name = "A";
+
+    public A(String name) {
+
+        System.out.println("para in A");
+
+    }
+
+    public static void print() {
+
+        System.out.println("in A = " + name);
+
+    }
+}
+
+class B extends A {
+
+    static String name = "B";
+
+    public B(String name) {
+
+        super(name);
+        System.out.println("parametr in B");
+
+    }
+
+    public static void print() {
+
+        System.out.println("in B = " + name);
+
+    }
 
 }
 
@@ -8,8 +38,14 @@ class TestA {
 
     public static void main(String[] args) {
 
-        A a = new A();
-        System.out.println(A.P);
+        B b = new B("bhanu");
+        System.out.println(B.name);
+
+        A a = new B("Beda");
+        System.out.println(A.name);
+
+        B.print();
+        A.print();
 
     }
 
