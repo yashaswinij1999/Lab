eb3908c33caeaa90d685a7bc8ee461a619899eb9
Yashaswini J <yashaswinij1999>
2023-08-12 10:25:14 +0530

    added

---
+++ Interface/Practice 2/A.java
@@ -0,0 +1,16 @@
+public class A {
+
+    public static final int P = 20;
+
+}
+
+class TestA {
+
+    public static void main(String[] args) {
+
+        A a = new A();
+        System.out.println(A.P);
+
+    }
+
+}
