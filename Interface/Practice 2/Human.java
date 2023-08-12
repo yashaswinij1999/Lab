2fb047e813f5fc07dc1a15236b754a1355acaea2
Yashaswini J <yashaswinij1999>
2023-08-12 12:56:18 +0530

    modified

---
+++ Interface/Practice 2/Human.java
@@ -0,0 +1,21 @@
+public final class Human {
+
+    private final int age;
+    private final String name;
+
+    public Human(int age, String name) {
+
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
