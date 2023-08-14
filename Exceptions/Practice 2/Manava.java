7492021c7d227495bbbd0c60098f1e3499b359ae
Yashaswini J <yashaswinij1999>
2023-08-14 18:53:26 +0530

    added

--- Exceptions/Practice 2/Manava.java
+++ Exceptions/Practice 2/Manava.java
@@ -33,15 +33,50 @@ public class Manava {
 
     }
 
+    public void dance(String type) throws DanceFailureException {
+
+        if (age > 50) {
+
+            if (type == "salsa") {
+                throw new DanceFailureException("Invalid type");
+            }
+
+        }
+
+    }
+
 }
 
 class TestM {
 
     public static void main(String[] args) {
 
-        Manava m = new Manava();
-        m.setAge(-1);
-        m.setName("");
+        try {
+
+            Manava m = new Manava();
+            m.setAge(59);
+            m.dance("salsa");
+
+        } catch (DanceFailureException e) {
+
+            // TODO: handle exception
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
+
+class DanceFailureException extends Exception {
+
+    public DanceFailureException() {
+
+    }
+
+    public DanceFailureException(String msg) {
+
+        super(msg);
 
     }
 
