7212323fea1bc84de513471e52e1b578b2b97f06
Yashaswini J <yashaswinij1999>
2024-04-03 15:57:44 +0530

    arithmetic operations

---
+++ Lab1/Practice  4/Qh.java
@@ -0,0 +1,22 @@
+
+public class Qh {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 4;
+        int result1 = x + y;
+        int result2 = x * y;
+        int result3 = x / y;
+        int result4 = x % y;
+        int result5 = x - y;
+
+        System.out.println(result1);//14
+        System.out.println(result2);//40
+        System.out.println(result3);//2
+        System.out.println(result4);//2
+        System.out.println(result5);//6
+
+    }
+
+}
