8ac81a772d7f502eefce76cc7546ec9415f2a768
Yashaswini J <yashaswinij1999>
2024-04-03 18:06:33 +0530

    printing numbers from 1 to 10 using for, while and do while lopp

---
+++ Lab1/Practice  4/Q4.java
@@ -0,0 +1,27 @@
+
+public class Q4 {
+
+    public static void main(String[] args) {
+
+        // printing using for loop
+        // for (int i = 1; i <= 10; i++) {
+        //     System.out.println(i);
+        // }
+        // using while loop
+        // int i = 1;
+        // while (i <= 10) {
+        //     System.out.println(i);
+        //     i++;
+        // }   
+
+        
+        // using do while
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+        
+    }
+
+}
