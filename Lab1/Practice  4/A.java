be51f76e67e0d0a95d18e5d5fd25e94f20434181
Yashaswini J <yashaswinij1999>
2024-04-03 15:35:38 +0530

    inital commit

---
+++ Lab1/Practice  4/A.java
@@ -0,0 +1,24 @@
+
+public class A {
+
+    public static void main(String[] args) {
+        System.out.println("A"
+        );
+    }
+
+}
+
+class B {
+
+    public static void main(String[] args) {
+        System.out.println("B");
+    }
+}
+
+class C {
+
+    public static void main(String[] args) {
+        System.out.println("C");
+    }
+
+}
