95bbba31cd84634627ffd6432c71b1a587049338
Yashaswini J <yashaswinij1999>
2024-04-03 18:19:26 +0530

    printing even and odd along with number

---
+++ Lab1/Practice  4/Q8.java
@@ -0,0 +1,18 @@
+
+public class Q8 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+
+            if (i % 2 == 0) {
+                System.out.println(" even " + i);
+            } else {
+                System.out.println(" odd " + i);
+            }
+
+        }
+
+    }
+
+}
