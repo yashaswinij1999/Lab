27cf3bb1838b49079a07183cd1899523010eda20
Yashaswini J <yashaswinij1999>
2024-04-03 17:54:19 +0530

    int to byte implicit conversion

---
+++ Lab1/Practice  4/Qm.java
@@ -0,0 +1,13 @@
+
+public class Qm {
+
+    public static void main(String[] args) {
+
+        byte a = 100;
+        byte b = 100;
+        byte c = (byte) (a + b);
+        System.out.println(c);
+
+    }
+
+}
