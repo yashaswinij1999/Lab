31f249d8947082ed551a8d843005c61085ad9348
Yashaswini J <yashaswinij1999>
2024-04-03 16:02:34 +0530

    tested different edge cases

---
+++ Lab1/Practice  4/Qg.java
@@ -0,0 +1,13 @@
+
+public class Qg {
+
+    public static void main(String[] args) {
+
+        System.out.println("1" + 2 + 3);
+        System.out.println(6 + 2 + "4");
+        System.out.println('2' + 2 + "2");
+        System.out.println('2' + 2);
+
+    }
+
+}
--- Lab1/Practice  4/Qh.java
+++ Lab1/Practice  4/Qh.java
@@ -4,18 +4,18 @@ public class Qh {
     public static void main(String[] args) {
 
         int x = 10;
-        int y = 4;
+        int y = 40;
         int result1 = x + y;
         int result2 = x * y;
         int result3 = x / y;
         int result4 = x % y;
         int result5 = x - y;
 
-        System.out.println(result1);//14
-        System.out.println(result2);//40
-        System.out.println(result3);//2
-        System.out.println(result4);//2
-        System.out.println(result5);//6
+        System.out.println(result1);//14,50
+        System.out.println(result2);//40,400
+        System.out.println(result3);//2,0
+        System.out.println(result4);//2,10
+        System.out.println(result5);//6,-30
 
     }
 
