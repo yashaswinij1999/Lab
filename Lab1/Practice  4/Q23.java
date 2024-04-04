bbc49624c8580064232bbc5c87feea8b6429fd0b
Yashaswini J <yashaswinij1999>
2024-04-04 14:09:10 +0530

    added

---
+++ Lab1/Practice  4/Q23.java
@@ -0,0 +1,13 @@
+
+public class Q23 {
+
+    public static void main(String[] args) {
+
+        int i = 50;
+        while (i != 0) {
+            System.out.println(i >>>= 1);
+        }
+
+    }
+
+}
