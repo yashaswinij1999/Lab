ed269c18f177c14230ae834d90f0bcdbd16fd61b
Yashaswini J <yashaswinij1999>
2024-04-04 14:06:07 +0530

    testing the control flow

---
+++ Lab1/Practice  4/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+
+    public static void process(int i) {
+
+        i = 20;
+        System.out.println(i);
+
+    }
+
+    public static void main(String[] args) {
+
+        int i = 10;
+        process(i);
+        System.out.println(i);
+
+    }
+}
