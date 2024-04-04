f195a9975864df5c42c426ff92e3a6d0741a33f2
Yashaswini J <yashaswinij1999>
2024-04-04 13:10:25 +0530

    testing inverse of a number

---
+++ Lab1/Practice  4/Q16.java
@@ -0,0 +1,14 @@
+
+public class Q16 {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        System.out.println(~x);
+
+        int a = 100;
+        System.out.println(~a);
+
+    }
+
+}
