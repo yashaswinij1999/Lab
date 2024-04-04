a25fff33d06995b8134330d1172760abdb0ea298
Yashaswini J <yashaswinij1999>
2024-04-04 12:58:35 +0530

    writtened a program to check remainder and quotient

---
+++ Lab1/Practice  4/Q11.java
@@ -0,0 +1,15 @@
+
+public class Q11 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int remainder = b % a;
+        int quotient = b / a;
+
+        System.out.println("the reminder : " + remainder + " quotient : " + quotient);
+
+    }
+
+}
