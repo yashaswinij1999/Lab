ff21dace6b9d73dbe2bf742d12d487635640466d
Yashaswini J <yashaswinij1999>
2024-04-04 14:12:39 +0530

    bitwise operations

---
+++ Lab1/Practice  4/Q17.java
@@ -0,0 +1,16 @@
+
+public class Q17 {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 6;
+        System.out.println(x | y);
+        System.out.println(x & y);
+        System.out.println(x ^ y);
+
+
+
+    }
+
+}
