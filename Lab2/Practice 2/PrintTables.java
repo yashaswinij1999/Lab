1f7bd2c7f8a03914bf51db8a884c925e8d7febac
Yashaswini J <yashaswinij1999>
2024-04-05 13:09:05 +0530

    done

---
+++ Lab2/Practice 2/PrintTables.java
@@ -0,0 +1,20 @@
+
+public class PrintTables {
+
+    public static void printTables(int num) {
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println(num + " * " + i + " = " + num * i);
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        
+        printTables(5);
+
+    }
+
+}
--- Lab2/Practice 2/Q4.java
+++ Lab2/Practice 2/Q4.java
@@ -5,6 +5,13 @@ public class Q4 {
         System.out.println(1 + 2 * 5 / 2 - 2 * 4);
         System.out.println(1 * 2 * 5 - 2 + 24);
         System.out.println(1 + 2 / 5 / 2 / 2 - 4);
+        System.out.println(true ? false : true);
+        System.out.println(true ? true : false);
+
+        char x = 'X';
+        int i = 0;
+        System.out.println(true ? x : 0);
+        System.out.println(false ? i : x);
     }
 
 }
