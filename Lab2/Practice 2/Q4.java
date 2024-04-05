2af90e37d2c5f189d592a44f939e0760e71b0ae6
Yashaswini J <yashaswinij1999>
2024-04-05 12:56:11 +0530

    checking precedenece

---
+++ Lab2/Practice 2/Q4.java
@@ -0,0 +1,10 @@
+
+public class Q4 {
+
+    public static void main(String[] args) {
+        System.out.println(1 + 2 * 5 / 2 - 2 * 4);
+        System.out.println(1 * 2 * 5 - 2 + 24);
+        System.out.println(1 + 2 / 5 / 2 / 2 - 4);
+    }
+
+}
