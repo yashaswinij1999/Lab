34e30d6766bb3de9575acb54e97669df195ea9eb
Yashaswini J <yashaswinij1999>
2024-04-06 13:58:40 +0530

    printing pattern

---
+++ Lab2/Practice 2/Q10.java
@@ -0,0 +1,22 @@
+
+public class Q10 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int k = 4; k >= i; k--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" *");
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+
+        }
+
+    }
+
+}
