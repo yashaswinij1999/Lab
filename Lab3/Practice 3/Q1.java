68dafb0d9c31df6ac63f52153099d56b57d6cce8
Yashaswini J <yashaswinij1999>
2024-04-06 14:17:52 +0530

    checking whether the given number is even or not

---
+++ Lab3/Practice 3/Q1.java
@@ -0,0 +1,19 @@
+
+public class Q1 {
+
+    public static void main(String[] args) {
+
+        System.out.println(isEven(55));
+    }
+
+    public static boolean isEven(int num) {
+
+        if (num % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+}
