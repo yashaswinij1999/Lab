5f59f43fd11d1cf2bb0c1698601341c08e1d5544
Yashaswini J <yashaswinij1999>
2024-04-06 13:30:16 +0530

    print the value given number of times if it is even or odd

---
+++ Lab2/Practice 2/Q8.java
@@ -0,0 +1,28 @@
+
+public class Q8 {
+
+    public static void main(String[] args) {
+
+        process(8);
+
+    }
+
+    public static void process(int num) {
+
+        if (num % 2 == 0) {
+
+            for (int i = 1; i <= num; i++) {
+                System.out.println("shamboo");
+            }
+
+        } else {
+
+            for (int i = 1; i <= num; i++) {
+                System.out.println("govinda");
+            }
+
+        }
+
+    }
+
+}
