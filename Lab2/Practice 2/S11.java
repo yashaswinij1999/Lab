0794cbdcb36af44270d2c8b5f574ff8d58ee3617
Yashaswini J <yashaswinij1999>
2024-04-06 14:06:46 +0530

    printing all characters from 1 to 65535

---
+++ Lab2/Practice 2/S11.java
@@ -0,0 +1,17 @@
+
+public class S11 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 65535; i++) {
+
+            if (i % 5 != 0) {
+                System.out.print((char) i);
+                System.out.print(" ");
+            }
+
+        }
+
+    }
+
+}
