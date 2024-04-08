ff884aea22e0f5d82ed02baa865c9fc4d32dbbc8
Yashaswini J <yashaswinij1999>
2024-04-08 19:58:42 +0530

    base 10 representaion on int number

--- Lab3/Practice 3/Q13.java
+++ Lab3/Practice 3/Q13.java
@@ -3,9 +3,13 @@ public class Q13 {
 
     public static void main(String[] args) {
 
-        int num = 16;
+        int num = 345;
         System.out.println(" the binary representation of number " + num + " is " + intToBinary(num));
         System.out.println(" the octal representation of number " + num + " is " + intToOctal(num));
+        System.out.println(" the octal representation of number " + num + " is " + intToHexa(num));
+        System.out.println(" the integer representation of binary number " + num + " is " + binToInt(num));
+        System.out.println(" the integer representation of oct number " + num + " is " + octToInt(num));
+        // binToInt(num);
 
     }
 
@@ -36,6 +40,42 @@ public class Q13 {
         }
 
         return lines;
+
+    }
+
+    public static String intToHexa(int num) {
+
+        String hexa = Integer.toHexString(num);
+        return hexa;
+
+    }
+
+    public static int binToInt(int num) {
+
+        String str = num + "";
+        int n = 0;
+        int r = 0;
+        int result = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            n = n + (int) (Math.pow(2, i) * Integer.parseInt(str.charAt(str.length() - 1 - i) + ""));
+
+        }
+        return n;
+    }
+
+    public static int octToInt(int num) {
+
+        String str = num + "";
+        int n = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            n = n + (int) (Math.pow(8, i) * Integer.parseInt(str.charAt(str.length() - 1 - i) + ""));
+
+        }
+        return n;
 
     }
 }
