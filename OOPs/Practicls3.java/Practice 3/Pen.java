83dec9895399780d167d77a88c2a543bad817689
Yashaswini J <yashaswinij1999>
2024-04-10 21:53:09 +0530

    pen implementation

---
+++ OOPs/Practicls3.java/Practice 3/Pen.java
@@ -0,0 +1,48 @@
+
+public class Pen {
+
+    int inkQty;
+    String color;
+
+    public void write(String data) {
+
+        if (inkQty > 0) {
+            System.out.println("Pen can write a data : " + data + " with " + color + " color ");
+        } else {
+            System.out.println("The ink qty is less, add ink to it");
+        }
+        inkQty--;
+
+    }
+
+    public void refill(int inkqty) {
+
+        if (inkqty > 0) {
+            inkQty = inkQty + inkqty;
+            System.out.println("The total inkQty : " + inkQty);
+        } else {
+            System.out.println("The ink qty provided is lesser than zero");
+        }
+
+    }
+
+}
+
+class TestPen {
+
+    public static void main(String[] args) {
+
+        Pen p = new Pen();
+        p.inkQty = 2;
+        p.color = "black";
+        p.write("rama");
+        p.refill(2);
+        p.write("rama");
+        p.write("sita");
+        p.write("hanuman");
+        p.write("shiva");
+        System.out.println(p.inkQty);
+
+    }
+
+}
