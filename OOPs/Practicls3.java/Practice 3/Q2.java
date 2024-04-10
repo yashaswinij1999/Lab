c5b06eb06c1ae3f1130aaa9cd51555e6fe2afd17
Yashaswini J <yashaswinij1999>
2024-04-10 21:03:34 +0530

    checking the default values of the instance varibles

---
+++ OOPs/Practicls3.java/Practice 3/Q2.java
@@ -0,0 +1,21 @@
+
+public class Q2 {
+
+    String str;
+    int i;
+    boolean b;
+
+}
+
+class TestQ2 {
+
+    public static void main(String[] args) {
+
+        Q2 q = new Q2();
+        System.out.println(q.str);
+        System.out.println(q.i);
+        System.out.println(q.b);
+
+    }
+
+}
