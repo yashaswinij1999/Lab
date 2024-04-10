4947c90402c59e282dde2d7d8a19d92cf3e8c007
Yashaswini J <yashaswinij1999>
2024-04-10 16:26:51 +0530

    creating cow object

---
+++ OOPs/Practicls3.java/Practice 3/Cow.java
@@ -0,0 +1,30 @@
+
+public class Cow {
+
+    String name;
+
+    public void moo() {
+        System.out.println("Cow mooing its name " + name);
+    }
+
+}
+
+class TestCow {
+
+    public static void main(String[] args) {
+
+        Cow c1 = new Cow();
+        c1.name = "Raani";
+        c1.moo();
+
+        Cow c2 = new Cow();
+        c2.name = "lakshmi";
+        c2.moo();
+
+        c1 = c2;
+        c1.name = "gowri";
+        c2.moo();
+
+    }
+
+}
