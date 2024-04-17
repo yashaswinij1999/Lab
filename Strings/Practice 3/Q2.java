c8ce5973f39efe8953dd46fdc6159375a50135a7
Yashaswini J <yashaswinij1999>
2024-04-17 16:45:54 +0530

    basic string testing

---
+++ Strings/Practice 3/Q2.java
@@ -0,0 +1,15 @@
+
+public class Q2 {
+
+    public static void main(String[] args) {
+
+        String s1 = "shivani";
+        String s2 = "shivani";
+        String s3 = new String("ahivani");
+        String s4 = new String("ganga");
+        String s5 = "ramaramarai";
+        String s6 = "ma";
+        System.out.println(s5.indexOf(6));
+    }
+
+}
