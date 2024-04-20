f53e17eab2b2bd597a8fa4e33ab5f11696add77c
Yashaswini J <yashaswinij1999>
2024-04-20 18:42:04 +0530

    storing random values in an array

---
+++ Array/Practice 3/Q3.java
@@ -0,0 +1,16 @@
+
+public class Q3 {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+
+            arr[i] = (int) (Math.random() * 100);
+            System.out.println(arr[i]);
+
+        }
+
+    }
+}
