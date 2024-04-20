fc178c84de89faa13a3a8186248fb2ae77c9a089
Yashaswini J <yashaswinij1999>
2024-04-20 18:35:41 +0530

    printing 1-10 elements in array

---
+++ Array/Practice 3/Q1.java
@@ -0,0 +1,15 @@
+
+public class Q1 {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[10];
+        for (int i = 1; i < 10; i++) {
+            arr[i] = i;
+            System.out.println(arr[i]);
+        }
+
+    }
+
+}
+
