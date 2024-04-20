a743f6faad4528e5ceb979a7dfcfa7b9f8f96f7b
Yashaswini J <yashaswinij1999>
2024-04-20 18:39:40 +0530

    storing values in  reverse order

---
+++ Array/Practice 3/Q2.java
@@ -0,0 +1,14 @@
+
+public class Q2 {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[10];
+
+        for (int i = 9; i > 0; i--) {
+            arr[i] = i;
+            System.out.println(arr[i]);
+        }
+
+    }
+}
