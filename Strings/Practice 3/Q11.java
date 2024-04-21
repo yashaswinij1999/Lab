c2aeb6dc77eda494bfed9ab2213e4cfaed4e5fff
Yashaswini J <yashaswinij1999>
2024-04-21 17:10:40 +0530

    testing using the string builder

---
+++ Strings/Practice 3/Q11.java
@@ -0,0 +1,12 @@
+
+public class Q11 {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("yashu");
+        System.out.println(sb.reverse());
+        System.out.println(sb.replace(0, 2, "su"));
+
+    }
+}
