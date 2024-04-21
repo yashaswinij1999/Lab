39bc2a886e33617d0378f0381ff066e61404e49e
Yashaswini J <yashaswinij1999>
2024-04-21 17:17:34 +0530

    swapping first and last characters of the string

---
+++ Strings/Practice 3/Q12.java
@@ -0,0 +1,13 @@
+
+public class Q12 {
+
+    public static void main(String[] args) {
+
+        String s = "welcome";
+        String str = s.charAt(s.length() - 1) + s.substring(1, s.length() - 1)+
+        s.charAt(0);
+        System.out.println(str);
+
+    }
+
+}
