9e4da18cc43be0d186d79259eb01d1301a6199db
Yashaswini J <yashaswinij1999>
2024-04-21 17:21:19 +0530

    swapping 2 varibles using third variable

---
+++ Strings/Practice 3/Q13.java
@@ -0,0 +1,18 @@
+
+public class Q13 {
+
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+        int temp = 0;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a + " " + b);
+
+    }
+
+}
