0486443cbde1de88c42510744532d355cb1a7ecd
Yashaswini J <yashaswinij1999>
2024-04-26 18:26:25 +0530

    created immutable class a tested it

---
+++ Optional Modifiers/Practice  2/Person.java
@@ -0,0 +1,23 @@
+
+public final class Person {
+
+    final int age;
+    final String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+}
+
+class TestPerson {
+
+    public static void main(String[] args) {
+
+        Person p = new Person(15, "rama");
+        System.out.println(p.name);
+
+    }
+
+}
