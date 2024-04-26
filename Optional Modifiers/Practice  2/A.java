3bbebed41928c8016b49e50b8a166d552889adf1
Yashaswini J <yashaswinij1999>
2024-04-26 18:22:26 +0530

    tested final optinal modifier

---
+++ Optional Modifiers/Practice  2/A.java
@@ -0,0 +1,21 @@
+
+public class A {
+
+    final int p;
+    public static final int R = 5;
+
+    public A(int p) {
+        this.p = p;
+    }
+}
+
+class TestA {
+
+    public static void main(String[] args) {
+
+        // A a = new A(15);
+        // System.out.println(a.p);
+        // System.out.println(a.R);
+    }
+
+}
