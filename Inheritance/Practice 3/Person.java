fe938991126f5e90dc9544f175f5d7ce8c06f041
Yashaswini J <yashaswinij1999>
2024-05-04 21:52:49 +0530

    implemented search functionality

--- Inheritance/Practice 3/Person.java
+++ Inheritance/Practice 3/Person.java
@@ -3,6 +3,39 @@ public class Person {
 
     private String name;
     private int age;
+    String[] petNames = new String[20];
+    int count = 0;
+
+    public void dance() {
+        if (age < 25) {
+            System.out.println("a person can dance chacha");
+        } else {
+            System.out.println("A person can dance waltz");
+        }
+    }
+
+    public void addPetNames(String name) {
+
+        if (count < petNames.length) {
+            petNames[count] = name;
+            count++;
+            System.out.println("name added successfully");
+        }
+    }
+
+    public String sing() {
+
+        String s = "";
+
+        for (int i = 0; i < petNames.length; i++) {
+
+            int random = (int) (Math.random() * petNames.length);
+            s += petNames[random];
+
+        }
+        return s;
+
+    }
 
     public void setName(String name) {
         if (name == null || name.equals("")) {
@@ -36,6 +69,16 @@ class TestPerson {
         System.out.println(p.getName());
         p.setAge(10);
         System.out.println(p.getAge());
+        p.dance();
+
+        p.addPetNames("rama");
+        p.addPetNames("shama");
+        p.addPetNames("bhama");
+        String song = p.sing();
+        System.out.println(song);
+
+
+    
 
     }
 
