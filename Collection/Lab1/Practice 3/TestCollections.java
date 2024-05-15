21d154d392de71683a0dfdc369371cc7fcebc695
Yashaswini J <yashaswinij1999>
2024-05-15 20:24:19 +0530

    iterating over the collections using the itreator object

--- Collection/Lab1/Practice 3/TestCollections.java
+++ Collection/Lab1/Practice 3/TestCollections.java
@@ -1,3 +1,34 @@
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
 public class TestCollections {
-    
+
+    public static void main(String[] args) {
+
+        Collection<String> arr = new ArrayList<String>();
+        arr.add("rama");
+        arr.add("sita");
+        arr.add("hanumantha");
+        arr.add("keshava");
+
+        // System.out.println(arr.remove("keshava"));
+        // System.out.println(arr);
+        // System.out.println(arr.contains("keshava"));
+        
+        Iterator it = arr.iterator();
+        while (it.hasNext()) {
+            Object elem = it.next();
+
+            if (elem instanceof String) {
+
+                String s = (String) elem;
+                System.out.println(s);
+            }
+
+        }
+
+    }
+
 }
