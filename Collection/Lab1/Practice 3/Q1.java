ea1bfbfd5e42e0eb2b367c08e7545695caacd2b5
Yashaswini J <yashaswinij1999>
2024-05-15 20:02:34 +0530

    checking 2  strings my their identity and equality

---
+++ Collection/Lab1/Practice 3/Q1.java
@@ -0,0 +1,13 @@
+public class Q1{
+
+    public static void main(String [] args){
+
+        String s1 = "rama";
+        String s2 = "rama";
+
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+
+    }
+
+}
