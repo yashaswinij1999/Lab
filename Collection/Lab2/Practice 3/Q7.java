12d6044e51b1b7e0fc0b32b510bdf6cf6177d579
Yashaswini J <yashaswinij1999>
2024-05-16 20:46:11 +0530

    removing duplicates with ordered and unordered collections

---
+++ Collection/Lab2/Practice 3/Q7.java
@@ -0,0 +1,25 @@
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class Q7 {
+
+    public static void main(String[] args) {
+
+        // String s = "I felt happy because I saw the others were happy and because I knew I should feel happy but I wasn't really happy";
+
+        // String[] split = s.split(" ");
+
+        // HashSet st = new HashSet<>();
+        // LinkedHashSet l = new LinkedHashSet<>();
+
+        // for (String str : split) {
+        //     st.add(str);
+        //     l.add(str);
+        // }
+        // // System.out.println(st);
+        // System.out.println(l);
+
+    }
+
+}
