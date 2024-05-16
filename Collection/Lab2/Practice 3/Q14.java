799d409a3912a9360da525e94d312344e6ebb49e
Yashaswini J <yashaswinij1999>
2024-05-16 20:51:58 +0530

    adding only unique elements

---
+++ Collection/Lab2/Practice 3/Q14.java
@@ -0,0 +1,35 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q14 {
+
+    public static void main(String[] args) {
+
+        String s = "this this is is a what how what is";
+
+        String[] str = s.split(" ");
+        List l = new ArrayList<>();
+        int count = 0;
+
+        for (int i = 0; i < str.length; i++) {
+
+            count = 0;
+
+            for (int j = 0; j < str.length; j++) {
+
+                if (str[i].equals(str[j])) {
+                    count++;
+                }
+
+            }
+            if (count == 1) {
+                l.add(str[i]);
+            }
+
+        }
+        System.out.println(l);
+
+    }
+
+}
