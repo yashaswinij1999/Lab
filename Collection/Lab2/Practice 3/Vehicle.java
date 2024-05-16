2dfdf0401d3cdcfd89e1a0ae153b536e23c7a143
Yashaswini J <yashaswinij1999>
2024-05-16 20:22:15 +0530

    overided the hashcode method

--- Collection/Lab2/Practice 3/Vehicle.java
+++ Collection/Lab2/Practice 3/Vehicle.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Vehicle {
 
@@ -17,6 +18,7 @@ public class Vehicle {
 
             Vehicle v = (Vehicle) o;
             if (this.name.equals(v.name) && this.capacity == v.capacity) {
+                System.out.println("In equals method");
                 return true;
             }
 
@@ -25,21 +27,27 @@ public class Vehicle {
 
     }
 
+    public int hashCode() {
+        System.out.println("in hashcode method");
+        return (name + capacity).hashCode();
+    }
+
 }
 
 class TestVehicle {
 
     public static void main(String[] args) {
 
-        Vehicle v1 = new Vehicle("Tata", 100);
-        Vehicle v2 = new Vehicle("Tata", 100);
-
-        System.out.println(v1.equals(v2));
-
-        ArrayList l = new ArrayList<>();
-        l.add(v1);
-        System.out.println(l.contains(v1));
-
+        // Vehicle v1 = new Vehicle("Tata", 100);
+        // Vehicle v2 = new Vehicle("Tata", 100);
+        // System.out.println(v1.equals(v2));
+        // ArrayList l = new ArrayList<>();
+        // l.add(v1);
+        // System.out.println(l.contains(v1));
+        // HashSet s = new HashSet<>();
+        // s.add(v1);
+        // s.add(v2);
+        // System.out.println(s);
     }
 
 }
