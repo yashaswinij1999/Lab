c8eb6dbc4b07c80960a1e7b33832a75dad34e66e
Yashaswini J <yashaswinij1999>
2024-05-24 20:05:54 +0530

    printing non unique words ina sentence

---
+++ Collection/Lab4/Practice 4/Q4.java
@@ -0,0 +1,35 @@
+
+import java.util.ArrayList;
+
+public class Q4 {
+
+    public static void main(String[] args) {
+
+        String s = "this this is is a what how what is";
+        String[] str = s.split(" ");
+        ArrayList<String> l1 = new ArrayList<>();
+        ArrayList<String> l2 = new ArrayList<>();
+        int count = 0;
+        String test = "";
+
+        for (int i = 0; i < str.length; i++) {
+
+            count = 0;
+
+            for (int j = 0; j < str.length; j++) {
+
+                if (str[i].equals(str[j])) {
+                    count++;
+
+                }
+
+            }
+            if(count > 1){
+                l1.add(str[i]);
+            }
+
+        }
+        System.out.println(l1);
+
+    }
+}
