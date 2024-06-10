8574d439bead245b32518cbf45873b86cb0ccdd9
Yashaswini J <yashaswinij1999>
2024-06-10 12:17:48 +0530

    generating the prime number

--- Threads/Practice 3/LetterCounterJob.java
+++ Threads/Practice 3/LetterCounterJob.java
@@ -53,22 +53,80 @@ public class LetterCounterJob implements Runnable {
 
 }
 
+class NextPrimeJob implements Runnable {
+
+    long input;
+    long outPut;
+
+    public NextPrimeJob(long input) {
+        this.input = input;
+    }
+
+    @Override
+    public void run() {
+
+        long rand = (long) (Math.random() * 10000000);
+        System.out.println("random number " + rand);
+
+        if (rand > input) {
+
+            if (rand <= 1) {
+                System.out.println("rand is lesse than one  hence its not prime");
+                return;
+            }
+
+            if (rand == 2 || rand == 3 || rand == 5 || rand == 7) {
+                this.outPut = rand;
+            }
+
+            if (rand % 2 == 0) {
+                System.out.println("rand is divisible by two, hence its not prime");
+                return;
+            }
+
+            for (int i = 3; i <= (int) (Math.sqrt(rand)); i += 2) {
+
+                if (rand % i == 0) {
+                    System.out.println("its not prime");
+                    return;
+                }
+
+            }
+            this.outPut = rand;
+
+        } else {
+            System.out.println("random number is less than input ");
+        }
+    }
+
+    public long getPrime() {
+        return this.outPut;
+    }
+
+}
+
 class TestLetterCounterJob {
 
     public static void main(String[] args) {
 
         try {
 
-            LetterCounterJob l = new LetterCounterJob("C:/Users/yashu/Desktop/Java/Java Labs/IO/Practice 3/IO Programming Questions/friends.txt");
-
-            Thread t = new Thread(l);
-            t.start();
-            t.join();
-            System.out.println(l.getCount());
+            // LetterCounterJob l = new LetterCounterJob("C:/Users/yashu/Desktop/Java/Java Labs/IO/Practice 3/IO Programming Questions/friends.txt");
+            // Thread t = new Thread(l);
+            // t.start();
+            // t.join();
+            // System.out.println(l.getCount());
+            NextPrimeJob j = new NextPrimeJob(2);
+            Thread t2 = new Thread(j);
+            t2.start();
+            t2.join();
+            System.out.println(j.getPrime());
 
         } catch (Exception e) {
+
             // TODO: handle exception
             e.printStackTrace();
+
         }
 
     }
